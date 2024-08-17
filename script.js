fbfa814226bdd1db5ec22f5ede324af90f0395e4
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2024-08-17 12:24:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+    const rows = document.querySelectorAll('#myTable tr');
+
+    rows.forEach(function(row, index) {
+        row.style.backgroundColor = index % 2 === 0 ? '#ffffff' : '#f2f2f2';
+    });
+});
